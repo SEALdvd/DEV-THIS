4efd3206e6757922d7ecb25b3303362a15d04cbb
SEALdvd <dannyvandalen@145-24-251-89.wlan.hro.nl>
2015-06-05 11:08:48 +0200

    index arduino pagina
    De virtuele arduino

---
+++ aindex.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dannyvandalen
+ * Date: 05-06-15
+ * Time: 09:57
+ */
+
+?>
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<button type="button" onclick="alert('Hello world!')">Click Me!</button>
+
+</body>
+</html>
+
